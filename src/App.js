f16032f1af31c6ba3d4faf470ef508a160d41d17
Alesh Houdek <mail@alesh.com>
2018-08-08 06:23:39 -0600

    style changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import ToDos from './components/ToDos/ToDos';
 
+import toDos from './services/DataService';
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-
+        <ToDos toDos={toDos}/>
       </div>
     );
   }
--- src/components/ToDos/ToDos.js
+++ src/components/ToDos/ToDos.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
 class ToDos extends Component {
+  // constructor (props) {
+  //   super(props);
+  // }
+
+  deleteItem = (event) => {
+    const toDos = this.props.toDos;
+    const deletedId = event.target.id;
+    console.log('deleted: ', this.props.toDos[deletedId].title);
+    toDos.splice(deletedId, 1);
+    this.props.setState({
+      toDos: toDos
+    });
+  }
+
   render() {
+    const list = (
+      this.props.toDos.map(todo => {
+        return (
+          <li className="ToDo" key={todo.id}>
+            <span onClick={this.deleteItem} id={todo.id} className="checkbox">✓</span>
+            {todo.title}
+          </li>
+        );
+      })
+    );
+
     return (
-      <p>ToDos working!</p>
+      <div className="ToDos">
+        <ul>
+          {list}
+        </ul>
+      </div>
     );
   }
 }
