308e6805f76ad4e626497a9fe1ea26bd7a75dab3
Alesh Houdek <mail@alesh.com>
2018-11-02 06:43:17 -0600

    Moved state to app, can delete items

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import ToDos from './components/ToDos/ToDos';
 import toDos from './services/DataService';
 
 class App extends Component {
+  state = {
+    toDos: toDos
+  };
+
+  deleteItem = e => {
+    const toDos = this.state.toDos;
+    const deletedId = e.target.id;
+    console.log('deleted: ', this.state.toDos[deletedId].title);
+    toDos.splice(deletedId, 1);
+    this.setState({
+      toDos: toDos
+    });
+  }
 
   render() {
     return (
       <div className="App">
-        <ToDos toDos={toDos}/>
+        <ToDos
+          toDos={this.state.toDos}
+          deleteItem={this.deleteItem}
+        />
       </div>
     );
   }
--- src/components/ToDos/ToDos.js
+++ src/components/ToDos/ToDos.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
 
 class ToDos extends Component {
-  // constructor (props) {
-  //   super(props);
-  // }
-
-  deleteItem = (event) => {
-    const toDos = this.props.toDos;
-    const deletedId = event.target.id;
-    console.log('deleted: ', this.props.toDos[deletedId].title);
-    toDos.splice(deletedId, 1);
-    this.props.setState({
-      toDos: toDos
-    });
-  }
-
   render() {
     const list = (
       this.props.toDos.map(todo => {
         return (
           <li className="ToDo" key={todo.id}>
-            <span onClick={this.deleteItem} id={todo.id} className="checkbox">✓</span>
+            <span onClick={this.props.deleteItem} id={todo.id} className="checkbox">✓</span>
             {todo.title}
           </li>
         );
